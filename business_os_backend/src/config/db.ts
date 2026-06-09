1e193f4884822785f8bc7c767dcbb18e5bdf358d
Yogeshwari M <your-email@gmail.com>
2026-06-09 15:34:27 +0530

    Remove src env file

--- business_os_backend/src/config/db.ts
+++ business_os_backend/src/config/db.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'business_os',
   waitForConnections: true,
   connectionLimit: 10,
